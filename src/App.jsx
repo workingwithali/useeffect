c69fd2d368db446c2cb1ab3554a52d9e24e068cb
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-01 12:34:00 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // On every render/refrech
+  useEffect (()=> {
+  alert("On every render/refrech")  
 
+  });
+ // On first render/refrech
+  useEffect (()=> {
+  alert("On first render/refrech")  
+
+  },[]);
+  // render when count change
+  useEffect (()=> {
+  alert("count was change")  
+
+  },[count]);
   return (
     <>
-      <div>
-      <h1>ali rashid</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <h1>the couunt was change{count}</h1>
+   <button onClick={()=>{setCount(count+1)}}>click me</button>
     </>
   )
 }
